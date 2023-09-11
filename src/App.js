f7c6b88ec83b89909dea6886b2cd5ce5e1424c9d
VladimirKoshovec <koshavladmir@gmail.com>
2023-09-11 09:19:49 +0300

    refactor button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import RequestForm from './components/RequestForm/RequestForm';
+import ReturnButtom from './components/ReturnButton/ReturnButton';
 
 function App() {
   return (
     <div className="App">
+      <ReturnButtom/>
       <RequestForm/>
     </div>
   );
---
+++ src/components/ReturnButton/ReturnButton.jsx
@@ -0,0 +1,13 @@
+const ReturnButtom = () => {
+    return (
+        <div>
+           <a href="#start" class="scroll">
+            <svg class="scroll__icon" width="30px" height="30px">
+               <use href="./images/icons.svg#icon-home"></use>
+            </svg>
+           </a>
+        </div>
+    )
+};
+
+export default ReturnButtom;
